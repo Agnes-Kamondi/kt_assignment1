7fd1c8410e27d31e3864a8c1d07aeaa82857f661
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-02-06 22:49:06 +0300

    created a kotlin file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import kotlin.Double
+import kotlin.Double as Double1
+
+fun main() {
+    var fullname: String = "Agnes Auma"
+    println("Full Name: Agnes Auma")
+    var age:Byte = 21
+    println("Age: 21")
+    var phoneNumber: String = "+254115052075"
+    println("Phone Number:+254115052075")
+    var weighKg: Double = 61.56
+    println("Weigh(in Kg): 61.5")
+    println("is a kenyan citizen")
+}
